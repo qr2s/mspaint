e61fffe36b75529de68e75f0ae58df3b299906d1
q.r2s <kncrypthuboffical@gmail.com>
2025-10-29 16:28:58 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/notpoiu/mspaint/releases/latest/download/Script.luau"))()
+loadstring(game:HttpGet("https://github.com/qr2s/mspaint/releases/latest/download/Script.luau"))()
